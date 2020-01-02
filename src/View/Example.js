526509298a61c9bb589bbb9da3f3b8051ae0d7ec
tiago marques <tiago@csktech.com.br>
2020-01-02 17:51:51 -0300

    contador

--- src/View/Example.js
+++ src/View/Example.js
@@ -1,19 +1,35 @@
 import React from "react"
 
 export class Examples extends React.Component {
-    state = {
-        count: 0
-    };
+    constructor(props){
+        super(props);
+        this.state = {
+            count: 0,
+            visible: false
+        };
+    }
+    
 
     Increment = () => {
-        this.setState({count: this.state.count + 1});
+        this.setState({
+            count: this.state.count + 1
+        });
     };
 
     Decrement = () => {
-        this.setState({count: this.state.count - 1})
+        const contador = this.state.count;
+
+        this.setState({
+            count: contador === 0
+                ? (contador + 0)
+                : (contador - 1)
+        })
     };
 
     render(){
+
+
+
         return (
             <div>
                 <h1>{this.state.count}</h1>
